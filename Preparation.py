9e9e47da80974b3a049b56283fef0dee3bc647e8
carsons11 <carsons11@nycstudents.net>
2025-10-06 08:59:09 -0400

    Update Preparation.py

--- Preparation.py
+++ Preparation.py
@@ -1,4 +1,9 @@
-def language(x):
+""" def language(x):
     print ("do something")
+language("Cadee's computer never seems to work in the morning") """
 
-language("Cadee's computer never seems to work in the morning")
+def lang():
+    input("Put your sentence here")
+
+
+
