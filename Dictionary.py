b765149e75a1780eb4e03a277847e293284a0db3
carsons11 <carsons11@nycstudents.net>
2025-10-09 09:15:43 -0400

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,9 +1,17 @@
 
-item1 = {"sad":"you"}
+""" item1 = {"sad":"you"}
 shopping = [
     "item1" = "happy";
 ]
-    
+     """
+
+student = {
+    'name':'Cadee',
+    'age' : '15',
+    'grades' : (80,90,100)
+
+}
+print (student['name'])
 
 
 
