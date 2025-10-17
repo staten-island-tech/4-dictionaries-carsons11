7246f1fd0d30715363265537199eef06e007cdea
carsons11 <carsons11@nycstudents.net>
2025-10-17 09:26:37 -0400

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -13,25 +13,33 @@ shopping = [
 }
 print (student['name']) """
 
-thrift_shop = [
-    {
+Thrift_shop = {
+"item1":{
         "name": "Basketball",
         "price": 29.99,
         "category": "sports",
     },
-    {
+"item2":{
         "name": "Action_figure",
         "price": 3.99,
         "category": "toys",
     },
-    {
+"item3":{
         "name": "Phone_charger",
         "price": 1.99,
         "category": "accessory",
     }
-]
-print ("Welcome to our shop. Here are the items that you can buy.")
-print (thrift_shop[2]["name"])
+}
+print ("Welcome to our fabulous thrift shop. Here are the items that you can buy.")
+for index, item in enumerate(Thrift_shop):
+    print (index, ":", item["name"])
+x = input("What would you like to buy?: ")
+for y in Thrift_shop:
+    if x == Thrift_shop[y]["name"]:
+        for z in Thrift_shop["item1"]:
+            print (Thrift_shop[y][z]) 
+
+
 
 
 
