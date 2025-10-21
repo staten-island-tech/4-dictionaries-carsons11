07d81fc2cfbd4f40d3c456f059b4d39efe98cb62
carsons11 <carsons11@nycstudents.net>
2025-10-21 09:26:33 -0400

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -14,30 +14,49 @@ shopping = [
 print (student['name']) """
 
 Thrift_shop = {
-"item1":{
+    "Basketball": {
         "name": "Basketball",
         "price": 29.99,
         "category": "sports",
     },
-"item2":{
-        "name": "Action_figure",
+    "Action Figure": {
+        "name": "Action Figure",
         "price": 3.99,
         "category": "toys",
     },
-"item3":{
-        "name": "Phone_charger",
+    "Phone Charger": {
+        "name": "Phone Charger",
         "price": 1.99,
         "category": "accessory",
     }
 }
 print ("Welcome to our fabulous thrift shop. Here are the items that you can buy.")
-for index, item in enumerate(Thrift_shop):
-    print (index, ":", item["name"])
-x = input("What would you like to buy?: ")
+for i in Thrift_shop:
+    print (Thrift_shop[i]['name'])
+cart = []
+price_cart = []
+a = input("Would you like to buy anything? Yes or No: ")
+while a == "Yes":    
+    b = input("What would you like to buy?")
+    cart.append(b)
+    price_cart.append(Thrift_shop[b]["price"])
+    print (f"This is your total: {price_cart}")
+    if a == "No":
+        print(sum(price_cart))
+    c = input("Would you like to buy anything else? Yes or No: ")
+if c == "Yes":
+    a == "Yes"
+elif c == "No":
+    a == "No"
+
+
+
+
+""" x = input("What would you like to buy?: ")
 for y in Thrift_shop:
     if x == Thrift_shop[y]["name"]:
         for z in Thrift_shop["item1"]:
-            print (Thrift_shop[y][z]) 
+            print (Thrift_shop[y][z])  """
 
 
 
