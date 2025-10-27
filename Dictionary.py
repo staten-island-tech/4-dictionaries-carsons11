b19ef562d4aba5552b8e388d77c1beeafe8856e8
carsons11 <carsons11@nycstudents.net>
2025-10-27 09:08:39 -0400

    Update Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -33,32 +33,26 @@ Thrift_shop = {
 print ("Welcome to our fabulous thrift shop. Here are the items that you can buy.")
 for i in Thrift_shop:
     print (Thrift_shop[i]['name'])
+a = ""
 cart = []
 price_cart = []
-a = input("Would you like to buy anything? Yes or No: ")
-while a == "Yes":    
-    b = input("What would you like to buy?")
-    cart.append(b)
-    price_cart.append(Thrift_shop[b]["price"])
-    print (f"This is your total: {price_cart}")
-    if a == "No":
-        print(sum(price_cart))
-    c = input("Would you like to buy anything else? Yes or No: ")
-if c == "Yes":
-    a == "Yes"
-elif c == "No":
-    print (f"This is ")
+while a != "No":
+    if a == "Yes":
+        b = input("What would you like to buy?")
+        cart.append(b)
+        price_cart.append(Thrift_shop[b]["price"])
+        print (f"This is your total: {sum(price_cart)}")
+    a = input("Would you like to buy anything? Yes or No: ")
+print(f"This is your price: {sum(price_cart)} and your cart: {(cart)}")
+    
+    
+
+        
+
 
     
 
 
-
-
-""" x = input("What would you like to buy?: ")
-for y in Thrift_shop:
-    if x == Thrift_shop[y]["name"]:
-        for z in Thrift_shop["item1"]:
-            print (Thrift_shop[y][z])  """
 
 
 
